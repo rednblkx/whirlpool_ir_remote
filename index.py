c2d3d186dc36b31b480ab2069f9c3a4242d145b7
White Red <rednblkx@protonmail.com>
2022-07-25 22:18:59 +0300

    functional pulse array generator

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
+import base64
 import ctypes
 from bitstring import BitArray
+import broadlink
+import datetime
 
 # Constants
 kWhirlpoolAcChecksumByte1 = 13
@@ -54,6 +57,66 @@ def xorBytes(code, length):
 
     return checksum
 
+def pad_hex_to_double_byte(bytes):
+    return bytes.zfill(4)
+
+def padHexToByte(byte):
+    return byte.zfill(2)
+
+def bigEndianToBigLittleDoubleHexByte(bytes):
+    return bytes[2:] + bytes[0:2]
+
+def pulesArrayToBroadlink(pulesArray):
+    pulse_array.insert(0, 0)
+    # clone a copy of the array
+    pulesArray = pulesArray[:]
+    # The boarding final command
+    broadlinkHexCommand = ''
+    # Get the frequency of the command
+    frequency = pulesArray.pop(0)
+    # whenever its a IR command
+    isIr = False
+    # If the frequency is default or 38/IR frequency
+    if frequency == 0 or frequency == 38:
+        isIr = True
+        broadlinkHexCommand +=  f'{38:x}'
+    else:
+        broadlinkHexCommand +=  f'{frequency:x}'
+    # The amount of pules
+    length = len(pulesArray)
+    # Add don't repeat byte flag
+    broadlinkHexCommand += '00'
+    # Get the length as hex value, (add 6 for the additional const broadlink command header)
+    hexLength =  f'{length + 6:x}'
+    # Pad the hex value to fill 2 bytes (for example for '2a' pad to '002a')
+    hexLengthByte = pad_hex_to_double_byte(hexLength)
+    # Convert the hex bytes to little endian (for example from '002a' to '2a00')
+    littleEndianLengthByte = bigEndianToBigLittleDoubleHexByte(hexLengthByte)
+    # Add the length bytes to the broadlink command
+    broadlinkHexCommand += littleEndianLengthByte
+    # Now convert all the pules
+    for pulesItem in pulesArray:
+        # Calculate the pules length (µs * 2^-15 formula)
+        puls = pulesItem * 269 / 8192
+        # Flat the pules (from xx.xx to xx)
+        flatPuls = int(puls)
+        # Convert the pules to hex
+        hexPuls =  f'{flatPuls:x}'
+        # the pules as broadlink hex
+        hexCommand = ''
+        # If the hex can stored in one byte
+        if len(hexPuls) <= 2:
+            # Pad the byte if needed (from 0x1 to '01')
+            hexCommand = padHexToByte(hexPuls)
+        else:
+            # Else if it can't stored in one byte, pad it to double byte
+            # and add the '00' flag (this flag mark that the next value is two bytes and not only one)
+            hexCommand = '00' + pad_hex_to_double_byte(hexPuls)
+        # Add the command to the final broadlink command
+        broadlinkHexCommand += hexCommand
+    # Return the final command, and add the IR command ends flag (for IR only)
+    return broadlinkHexCommand + (isIr and '000d05000000000000' or '')
+
 class WhirlpoolAC(ctypes.Structure):
     _pack_ = 1
     _fields_ = [
@@ -71,7 +134,7 @@ class WhirlpoolAC(ctypes.Structure):
         ("Super1", ctypes.c_uint8, 1),
         ("", ctypes.c_uint8, 2),
         ("Super2", ctypes.c_uint8, 1),
-        ("ClockHours", ctypes.c_uint8, 1),
+        ("ClockHours", ctypes.c_uint8, 5),
         ("LightOff", ctypes.c_uint8, 1),
         ("", ctypes.c_uint8, 2),
         ("ClockMins", ctypes.c_uint8, 6),
@@ -118,9 +181,10 @@ def setFan(speed):
         ac.Cmd = kWhirlpoolAcCommandFanSpeed
 
 def setSuper(super):
+    Mode = ac.Mode
     if (super):
         setFan(kWhirlpoolAcFanHigh)
-        match ac.Mode:
+        match Mode:
             case 0:
                 setTemp(kWhirlpoolAcMaxTemp)
             case 2:
@@ -179,57 +243,51 @@ def checksum():
     if (21 >= kWhirlpoolAcChecksumByte2):
         ac.Sum2 = xorBytes(bytes(ac)[kWhirlpoolAcChecksumByte1 + 1:],
         kWhirlpoolAcChecksumByte2 - kWhirlpoolAcChecksumByte1 - 1)
-
-
-def irCode(headermark, headerspace, onemark, onespace, zeromark, zerospace, footermark, gap, data, nbytes):
-    # Header
-    print(headermark, end=', ')
-    print(headerspace, end=', ')
-    # Data
-    for i in range(0, nbytes):
-        # data >>= 1
-        # print(int(data[i]) & 1)
-        if (int(bytes(ac)[i]) & 1):
-            print(onemark, end=', ')
-            print(onespace, end=', ')
-        else:
-            print(zeromark, end=', ')
-            print(zerospace, end=', ')
-    # data >>= 1
-    # Footer
-    print(footermark, end=', ')
-    print(gap)
-
+now = datetime.datetime.now()
 setMode(kWhirlpoolAcCool)
-
-
 setSwing(False)
-
 setLight(True)
-
-
-
 setFan(0)
 setTemp(18)
-setClock(1,22)
-
+setClock(now.hour, now.minute)
 setPower(True)
-
 checksum()
-
 print(bytes(ac))
-
-
 c = BitArray(bytes=bytes(ac))
-
 l = bytearray(21)
-
 value = int.from_bytes(ac, byteorder='big')
-print(bin(value))
-for i in range(0, 6):
-    value >>= 1
-    print(bin(value))
-# print(c & l)
 
-irCode(kWhirlpoolAcHdrMark, kWhirlpoolAcHdrSpace, kWhirlpoolAcBitMark, kWhirlpoolAcOneSpace, kWhirlpoolAcBitMark, kWhirlpoolAcZeroSpace, kWhirlpoolAcBitMark, kWhirlpoolAcGap, c, 6)
+pulse_array = [kWhirlpoolAcHdrMark, kWhirlpoolAcHdrSpace]
 
+def pulse_codes(dataptr, nbytes, end):
+  result = []
+  for i in range(nbytes):
+    data = dataptr[i]
+    for _ in range(8):
+      if (data & 1):
+        result.append(kWhirlpoolAcBitMark)
+        result.append(kWhirlpoolAcOneSpace)
+      else:
+        result.append(kWhirlpoolAcBitMark)
+        result.append(kWhirlpoolAcZeroSpace)
+      data >>= 1
+    result.append(kWhirlpoolAcBitMark)
+    result.append(kWhirlpoolAcGap)
+  return result
+
+pulse_array.extend(pulse_codes(bytearray(ac), 6, False))
+pulse_array.extend(pulse_codes(bytearray(ac)[6:], 8, False))
+pulse_array.extend(pulse_codes(bytearray(ac)[14:], 7, True))
+
+# base64_string = base64.b64encode(bytes.fromhex(pulesArrayToBroadlink(pulse_array)))
+
+# print(base64_string)
+
+hex = pulesArrayToBroadlink(pulse_array)
+
+# Sending the IR command to the Broadlink device.
+device = broadlink.hello('10.0.0.35')  # IP address of your Broadlink device.
+
+device.auth()
+
+# device.send_data(bytes.fromhex(hex))
